33894a9d929f7aa6f5f21d7dbf91917645450016
Rui Pedro Rodrigues <rui.pedro.r@gmail.com>
2013-02-11 19:55:18 +0000

    5. Using Handlebar Templates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 var app = {
 	renderHomeView: function() {
-		var html =
-				"<div class='header'><h1>Home</h1></div>" +
-				"<div class='search-view'>" +
-				"<input class='search-key'/>" +
-				"<ul class='employee-list'></ul>" +
-				"</div>"
-		$('body').html(html);
+		$('body').html(this.homeTpl());
 		$('.search-key').on('keyup', $.proxy(this.findByName, this));
 	},
 
@@ -18,25 +12,21 @@ var app = {
 		}
 	},
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+	findByName: function() {
+		var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
+		});
+	},
     
     initialize: function() {
-    var self = this;
-    this.store = new MemoryStore(function() {
-        self.renderHomeView();
-    });
-}
+		var self = this;
+		this.store = new MemoryStore(function() {
+			self.renderHomeView();
+		});
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+	}
 
 };
 
