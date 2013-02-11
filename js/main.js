6771a733a7e5bb22d851dc6c96e1b828246b21d3
Rui Pedro Rodrigues <rui.pedro.r@gmail.com>
2013-02-11 19:33:37 +0000

    3. Using Native Notification

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 var app = {
 
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -14,8 +22,10 @@ var app = {
     },
     
     initialize: function() {
-        //this.store = new MemoryStore();
-        this.store = new WebSqlStore();
+		var self = this;
+        this.store = new MemoryStore(function () {
+			self.showAlert('Store initialized', 'Info');
+		});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
