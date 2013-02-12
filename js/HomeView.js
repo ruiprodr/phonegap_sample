182bc0a57ef782ce18345401cfce3d9ec10677f4
Rui Pedro Rodrigues <rui.pedro.r@gmail.com>
2013-02-12 12:48:22 +0000

    7. Adding Styles and Touch-Based Scrolling

--- js/HomeView.js
+++ js/HomeView.js
@@ -12,9 +12,16 @@ var HomeView = function(store) {
 	};
 
 	this.findByName = function() {
-	    store.findByName($('.search-key').val(), function(employees) {
-	        $('.employee-list').html(HomeView.liTemplate(employees));
-	    });
+		store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(HomeView.liTemplate(employees));
+			if (self.iscroll) {
+				console.log('Refresh iScroll');
+				self.iscroll.refresh();
+			} else {
+				console.log('New iScroll');
+				self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+			}
+		});
 	};
  
 	this.initialize();
