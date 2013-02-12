fa8cbb879b9f75bc0c660c9e81fdbeadef2e881d
Rui Pedro Rodrigues <rui.pedro.r@gmail.com>
2013-02-12 18:49:28 +0000

    12. Using the Camera API

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -4,6 +4,7 @@ var EmployeeView = function(employee) {
 		this.el = $('<div/>');
 		this.el.on('click', '.add-location-btn', this.addLocation);
 		this.el.on('click', '.add-contact-btn', this.addToContacts);
+		this.el.on('click', '.change-pic-btn', this.changePicture);
 	};
  
 	 this.render = function() {
@@ -38,6 +39,30 @@ var EmployeeView = function(employee) {
 		phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
 		contact.phoneNumbers = phoneNumbers;
 		contact.save();
+		return false;
+	};
+
+	this.changePicture = function(event) {
+		event.preventDefault();
+		if (!navigator.camera) {
+			app.showAlert("Camera API not supported", "Error");
+			return;
+		}
+		var options =   {   quality: 50,
+							destinationType: Camera.DestinationType.DATA_URL,
+							sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+							encodingType: 0     // 0=JPG 1=PNG
+						};
+	 
+		navigator.camera.getPicture(
+			function(imageData) {
+				$('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+			},
+			function() {
+				app.showAlert('Error taking picture', 'Error');
+			},
+			options);
+	 
 		return false;
 	};
 
