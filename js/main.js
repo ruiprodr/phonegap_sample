6839913c995018362bf6c246e712067d1f19d938
Rui Pedro Rodrigues <rui.pedro.r@gmail.com>
2013-02-12 19:06:15 +0000

    13. Sliding Pages with CSS Transitions

--- js/main.js
+++ js/main.js
@@ -32,17 +32,62 @@ var app = {
 	},
 
 	route: function() {
+		var self = this;
 		var hash = window.location.hash;
 		if (!hash) {
-			$('body').html(new HomeView(this.store).render().el);
+			if (this.homePage) {
+				this.slidePage(this.homePage);
+			} else {
+				this.homePage = new HomeView(this.store).render();
+				this.slidePage(this.homePage);
+			}
 			return;
 		}
-		var match = hash.match(app.detailsURL);
+		var match = hash.match(this.detailsURL);
 		if (match) {
 			this.store.findById(Number(match[1]), function(employee) {
-				$('body').html(new EmployeeView(employee).render().el);
+				self.slidePage(new EmployeeView(employee).render());
 			});
 		}
+	},
+	
+	slidePage: function(page) {
+	 
+		var currentPageDest,
+			self = this;
+	 
+		// If there is no current page (app just started) -> No transition: Position new page in the view port
+		if (!this.currentPage) {
+			$(page.el).attr('class', 'page stage-center');
+			$('body').append(page.el);
+			this.currentPage = page;
+			return;
+		}
+	 
+		// Cleaning up: remove old pages that were moved out of the viewport
+		$('.stage-right, .stage-left').not('.homePage').remove();
+	 
+		if (page === app.homePage) {
+			// Always apply a Back transition (slide from left) when we go back to the search page
+			$(page.el).attr('class', 'page stage-left');
+			currentPageDest = "stage-right";
+		} else {
+			// Forward transition (slide from right)
+			$(page.el).attr('class', 'page stage-right');
+			currentPageDest = "stage-left";
+		}
+	 
+		$('body').append(page.el);
+	 
+		// Wait until the new page has been added to the DOM...
+		setTimeout(function() {
+			// Slide out the current page: If new page slides from the right -> slide current page to the left, and vice versa
+			$(self.currentPage.el).attr('class', 'page transition ' + currentPageDest);
+			// Slide in the new page
+			$(page.el).attr('class', 'page stage-center transition');
+			self.currentPage = page;
+		});
+	 
 	},
 
 	initialize: function() {
