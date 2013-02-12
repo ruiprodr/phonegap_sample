302c5338c8086039749aa38344c60c2b2fdfe0dd
Rui Pedro Rodrigues <rui.pedro.r@gmail.com>
2013-02-12 14:59:22 +0000

    9. View Routing

---
+++ js/EmployeeView.js
@@ -0,0 +1,15 @@
+var EmployeeView = function() {
+ 
+	this.initialize = function() {
+		this.el = $('<div/>');
+	};
+ 
+	 this.render = function() {
+		this.el.html(EmployeeView.template(employee));
+		return this;
+	};
+	
+	this.initialize();
+}
+
+EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ var app = {
 				$(event.target).removeClass('tappable-active');
 			});
 		}
+		$(window).on('hashchange', $.proxy(this.route, this));
 	},
 
 	showAlert: function (message, title) {
@@ -29,12 +30,27 @@ var app = {
 			alert(title ? (title + ": " + message) : message);
 		}
 	},
-    
+
+	route: function() {
+		var hash = window.location.hash;
+		if (!hash) {
+			$('body').html(new HomeView(this.store).render().el);
+			return;
+		}
+		var match = hash.match(app.detailsURL);
+		if (match) {
+			this.store.findById(Number(match[1]), function(employee) {
+				$('body').html(new EmployeeView(employee).render().el);
+			});
+		}
+	},
+
 	initialize: function() {
 	    var self = this;
+		this.detailsURL = /^#employees\/(\d{1,})/;
 		this.registerEvents();
 	    this.store = new MemoryStore(function() {
-	        $('body').html(new HomeView(self.store).render().el);
+	        self.route();
 	    });
 	}
 
